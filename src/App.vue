a428b60523ee043a515407a2c5ef2a1343d3141c
navan <contactnavaneeth@gmail.com>
2024-08-13 14:25:26 +1000

    provide/inject vs defineProps

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <script setup>
-import PersonalDetails from './components/PersonalDetails.vue'
-import ProfessionalInfo from './components/ProfileDetails.vue'
+import PersonalDetails from "./components/PersonalDetails.vue"
+import ProfileDetails from "./components/ProfileDetails.vue"
+import {provide} from 'vue'
 
+provide('abtalt',"About navan")
 </script>
 
 <template>
   <header>
-    <img alt="navan logo" class="logo" src="./assets/navan.jpg" />
+   <img alt="navan logo" class="logo" src="./assets/navan.jpg" />
 
     <div class="wrapper">
-      <PersonalDetails name="Navan Sundarrajan" blurb="This is navan's page" />
+      <PersonalDetails name="Navan Sundarrajan" blurb="This is my page" />
     </div>
   </header>
 
   <main>
-    <ProfessionalInfo name="Navan's Profile" />
+    <ProfileDetails />
   </main>
-
-  <footer>
-    
-  </footer>
 </template>
 
 <style scoped>
@@ -30,9 +28,9 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+  width:125px;
+  height:125px;
   border-radius:10px;
-  height: 8rem;
-  width:8rem;
 }
 
 @media (min-width: 1024px) {
@@ -44,9 +42,9 @@ header {
 
   .logo {
     margin: 0 2rem 0 0;
+    width:125px;
+    height:125px;
     border-radius:10px;
-    height: 8rem;
-    width:8rem;
   }
 
   header .wrapper {
--- src/components/icons/IconAboutme.vue
+++ src/components/icons/IconAboutme.vue
@@ -1,9 +1,13 @@
+<script setup>
+import {inject} from 'vue'
+
+const abtAltTag=inject('abtalt')
+</script>
 <template>
-  <img src='../../assets/aboutme.jpg'  class='thumbnail' />
+  <img src='../../assets/aboutme.jpg'  class='thumbnail' :alt=abtAltTag />
 </template>
 
 <style scoped>
-
 .thumbnail {
   width:30px;
   height:30px;
