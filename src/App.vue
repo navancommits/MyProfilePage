c6b0c51de0314bbda7042da0a902497bf51ebb60
navan <contactnavaneeth@gmail.com>
2024-08-14 23:51:35 +1000

    component tag and dynamic component allocation

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,12 @@
 <script setup>
-import PersonalDetails from "./components/PersonalDetails.vue"
-import ProfileDetails from "./components/ProfileDetails.vue"
-import {provide} from 'vue'
-
-provide('abtalt',"About navan")
+import LeftImageComponent from "./components/LeftImageComponent.vue"
+import RightImageComponent from "./components/RightImageComponent.vue"
 </script>
 
 <template>
-  <header>
-   <img alt="navan logo" class="logo" src="./assets/navan.jpg" />
-
-    <div class="wrapper">
-      <PersonalDetails name="Navan Sundarrajan" blurb="This is my page" />
-    </div>
-  </header>
-
-  <main>
-    <ProfileDetails />
-  </main>
+  <component :is="Math.random()>0.5?LeftImageComponent:RightImageComponent" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-  width:125px;
-  height:125px;
-  border-radius:10px;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-    width:125px;
-    height:125px;
-    border-radius:10px;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/LeftImageComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+import PersonalDetails from "./PersonalDetails.vue"
+import ProfileDetails from "./ProfileDetails.vue"
+import {provide} from 'vue'
+
+provide('abtalt',"About navan")
+</script>
+
+<template>
+  <personal>
+   <img alt="navan logo" class="logo" src="../assets/navan.jpg" />
+
+    <div class="wrapper">
+      <PersonalDetails name="Navan Sundarrajan" blurb="This is my page" />
+    </div>
+  </personal>
+
+  <main>
+    <ProfileDetails />
+  </main>
+</template>
+
+<style scoped>
+personal {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  width:125px;
+  height:125px;
+  border-radius:10px;
+}
+
+@media (min-width: 1024px) {
+  personal {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+    width:125px;
+    height:125px;
+    border-radius:10px;
+  }
+
+  personal .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
