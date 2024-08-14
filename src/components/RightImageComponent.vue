772535deb88457c8385bc095f2983198bb63e8a4
navan <contactnavaneeth@gmail.com>
2024-08-14 23:57:03 +1000

    added margin left for right image component

--- src/components/RightImageComponent.vue
+++ src/components/RightImageComponent.vue
@@ -36,7 +36,7 @@ personal {
   personal {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding-left: calc(var(--section-gap) / 2);
   }
 
   .logo {
